fb7b3e2577319c8c76408972f25f20f7231fe12a
jedapaji <jessicadpaez@outlook.com>
2023-02-06 18:36:48 -0500

    Ajuste main y html

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,53 +1,4 @@
-/*const API =
-"https://youtube-v31.p.rapidapi.com/search?channelId=UCb2HGwORFBo94DmRx4oLzow&part=snippet%2Cid&order=date&maxResults=50";
-
-const content = null || document.getElementById("content");
-
-const options = {
-method: "GET",
-headers: {
-  "X-RapidAPI-Key": "26fe4b34b5mshc6bc2816f8ad626p1a04dcjsnb143a8835af8",
-  "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
-},
-};
-
-async function fetchData(urlApi) {
-const response = await fetch(urlApi, options);
-const data = await response.json();
-return data;
-}
-
-(async () => {
-try {
-  const videos = await fetchData(API);
-  let view = `
-  ${videos.items
-    .map(
-      (video) => `
-    <div class="group relative">
-    <div
-        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-    </div>
-    <div class="mt-4 flex justify-between">
-        <h3 class="text-sm text-gray-700">
-            <span aria-hidden="true" class="absolute inset-0"></span>
-            ${video.snippet.title}
-        </h3>
-    </div>
-</div>
-  `
-    )
-    .slice(0, 8)
-    .join("")}
-  `;
-  content.innerHTML = view;
-} catch (error) {
-  console.log(error);
-}
-})();*/
-const API =
-"https://deezerdevs-deezer.p.rapidapi.com/playlist/8874807802";
+const API = "https://deezerdevs-deezer.p.rapidapi.com/playlist/8874807802";
 
 const content = null || document.getElementById("content");
 
@@ -68,21 +19,20 @@ return data;
 (async () => {
 try {
 
-  const pokemons = await fetchData(API);
-  console.log('esto es pokemons.tracks.data',pokemons.tracks.data)
+  const artists = await fetchData(API);
   let view = `
-  ${pokemons.tracks.data
+  ${artists.tracks.data
     .map(
-      (pokemon) => `
+      (artist) => `
     <div class="group relative">
     <div
         class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-        <img src="${pokemon.album.cover_medium}" alt="${pokemon.title}" class="w-full">
+        <img src="${artist.album.cover_medium}" alt="${artist.title_short}" class="w-full">
     </div>
     <div class="mt-4 flex justify-between">
         <h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"></span>
-            ${pokemon.title_short}
+            ${artist.title}
         </h3>
     </div>
 </div>
